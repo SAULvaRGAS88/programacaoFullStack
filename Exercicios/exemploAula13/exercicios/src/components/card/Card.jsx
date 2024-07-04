2c309c1467486b66ba9f5a9906f2cc38a97ce28e
Saul <saulvargascosta@hotmail.com>
2024-07-03 21:56:41 -0300

    aula

--- Exercicios/exemploAula13/exercicios/src/components/card/Card.jsx
+++ Exercicios/exemploAula13/exercicios/src/components/card/Card.jsx
@@ -13,7 +13,7 @@ const Card = ({ produto }) => {
         <div className="card">
             <h2>{produto.nome}</h2>
             <p><strong>ID:</strong> {produto.id}</p>
-            <p><strong>Preço:</strong> R$ {produto.preco?.toFixed(2)}</p>
+            <p><strong>Preço:</strong> R$ {produto.preco.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}</p>
             <p><strong>Descrição:</strong> {produto.descricao}</p>
             <img src={produto.imagem} alt={produto.nome} width={150}></img>
             <button onClick={() => excluirCard(produto.id)}>Excluir</button>
